44584e75cb28e49edc558479e0f2b0778ce0f601
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-10 15:57:42 +0300

    Update async-await.ts

--- async-await.ts
+++ async-await.ts
@@ -1,1 +1,9 @@
-
+async function getDataAsync(){
+  try{
+    const data:string = await fetchDataPromise();
+    console.log(data);
+  }catch (error){
+    console.log((error as Error).message);
+  }
+}
+getDataAsync;
