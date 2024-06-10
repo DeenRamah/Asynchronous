4b07d43e8660a32606139cd286dbbff6fa14a318
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-10 16:17:57 +0300

    Update fetch-Api.ts

--- fetch-Api.ts
+++ fetch-Api.ts
@@ -1,1 +1,28 @@
+import fetch from 'node-fetch';
 
+//function to fetch data from an API
+async function fetchApiData (url: string): Promise<any>{
+
+  const response = await fetch(apiUrl);
+  if(!response.ok){
+    throw new Error("Network was not ok");
+  }
+  return response.json();
+}
+
+//Function to display data
+function displayData(data: any): void{
+  console.log(data);
+}
+
+async function fetchAndDisplayData(){
+  const apiUrl =".....";
+  try{
+    const data = await fetchApiData(apiUrl);
+    displayData(data);
+  }catch(error){
+    console.error("error",(error as error);
+  }
+}
+
+fetchAndDisplayDat();
