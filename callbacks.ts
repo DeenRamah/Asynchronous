c36a43777f709b67672a06af2359bd18a31ddc0b
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-10 14:34:03 +0300

    Update callbacks.ts

--- callbacks.ts
+++ callbacks.ts
@@ -1,1 +1,20 @@
 
+function fetchDataCallback(callback: (error: Error | null, data?: string) => void) {
+    setTimeout(() =>{
+        const isSuccess = Math.random() > 0.5;
+        if (isSuccess) {
+            callback(null, "Data Fetched success");
+        } else{
+            callback(new Error("Failed to fetch data"));
+        }
+}, 2000);
+}
+
+// using the function with a callback
+fetchDataCallback((error, data) =>{
+    if(error){
+    console.log("Callback:", error.message);
+   }else {
+    console.log("Callback:", data);
+    }
+});
