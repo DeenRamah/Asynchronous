98c41a5701a0be9fc7db8c69c963608da3e92c8f
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-10 15:43:56 +0300

    Update Promises.ts

--- Promises.ts
+++ Promises.ts
@@ -1,1 +1,22 @@
 
+function fetchDatapromis(): Promise<string>{
+  return new Promise((resolve, reject) =>{
+    const isSuccess = Math.Random() >0.5;
+    if(isSuccess){
+      resolve("Data Fetched successfully!");
+    }else {
+      reject(new Error("Failed to fetch data"));
+    }
+  }, 2000);
+});
+}
+
+
+//using the function with Promises
+fetchDataPromise()
+.then(data=>{
+  console.log(data);
+})
+.catch(error => {
+  console.error(error.message);
+});
